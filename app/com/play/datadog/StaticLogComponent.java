d39f0ea2e011457fa6bc1d4a5c539363e200876b
Alexander Okulovich <aokulovich@stiltsoft.com>
2021-05-21 18:25:10 +0300

    class with static object init fails too

--- app/com/play/datadog/StaticLogComponent.java
+++ app/com/play/datadog/StaticLogComponent.java
@@ -7,10 +7,15 @@ import javax.inject.Singleton;
 @Singleton
 public class StaticLogComponent {
 
-    private static final Logger.ALogger log = Logger.of(StaticLogComponent.class);
+    private final Logger.ALogger log = Logger.of(this.getClass());
+    // private static final Logger.ALogger log = Logger.of(StaticLogComponent.class);
+    // OR
+    private static final SomeObject SOME_OBJECT = new SomeObject();
 
     public void writeToLog() {
         log.warn("This is static logger message");
     }
 
+    private static class SomeObject {
+    }
 }
